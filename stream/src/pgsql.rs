4bf69247bb0f62e7330402c409b0709dbca84fa6
fcracker79 <fcracker79@gmail.com>
2019-07-17 21:02:03 +0200

    Working events stream

--- stream/src/pgsql.rs
+++ stream/src/pgsql.rs
@@ -6,34 +6,10 @@ pub mod pgsql {
     use postgres::{Connection, TlsMode};
     use std::boxed::Box;
     use fallible_iterator::FallibleIterator;
-    use serde_json::{Result, Value};
-
-
-    pub struct PostgreSQLStreamProducer<'a> {
-        url: &'a str,
-        table_name: &'a str,
-        column_name: &'a str,
-        filter: &'a str
-    }
-    
-    impl <'a> PostgreSQLStreamProducer<'a> {
-        pub fn new(url: &'a str, table_name: &'a str, column_name: &'a str, filter: &'a str) -> PostgreSQLStreamProducer<'a> {
-            PostgreSQLStreamProducer{url: url, table_name: table_name, column_name: column_name, filter: filter}
-        }
-    }
-    impl <'a> stream::StreamProducer for PostgreSQLStreamProducer<'a> {
-        fn produce(& self, consumer: &mut impl stream::StreamConsumer) {
-            let sql = &format!("SELECT {} FROM {}  WHERE {}", self.column_name, self.table_name, self.filter);
-            let conn = Connection::connect(self.url, TlsMode::None).unwrap();
-            for next_row in &conn.query(sql, &[]).unwrap() {
-                let data: String = next_row.get(0);
-                let bin_data : &[u8] = data.as_bytes();
-                consumer.write(stream::SourceElement{id: Box::from("123"), data: Box::from(bin_data)});
-            }
-            consumer.flush();
-        }
-    }
-
+    use serde_json::{Value};
+    use std::time::{SystemTime, Duration};
+    const NOTIFY_TIMEOUT : Duration = Duration::from_secs(60);
+    const NOTIFY_TIMEOUT_1 : Duration = Duration::from_secs(3);
     pub struct PostgreSQLListenStreamProducer<'a> {
         url: &'a str,
         table_name: &'a str,
@@ -46,38 +22,58 @@ pub mod pgsql {
             PostgreSQLListenStreamProducer{url: url, table_name: table_name, column_name: column_name, channel: channel}
         }
     }
-
+    
+    impl <'a> PostgreSQLListenStreamProducer<'a> {
+        fn flush_consumer(&self, data_to_delete: &mut Vec<i32>, consumer: &mut impl stream::StreamConsumer, conn: &Connection) {
+            consumer.flush();
+            if !data_to_delete.is_empty() {
+                conn.execute(
+                    &format!(
+                        "DELETE FROM {} WHERE id IN ({})", 
+                        self.table_name, 
+                        data_to_delete.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(",")
+                    ), 
+                    &[]
+                ).unwrap();
+            }
+                data_to_delete.clear();
+        }
+    }
     impl <'a> stream::StreamProducer for PostgreSQLListenStreamProducer<'a> {
         fn produce(& self, consumer: &mut impl stream::StreamConsumer) {
             let conn = Connection::connect(self.url, TlsMode::None).unwrap();
             let notifications = conn.notifications();
-            let mut it = notifications.iter();
+            let mut it = notifications.timeout_iter(NOTIFY_TIMEOUT_1);
             conn.execute(&*format!("LISTEN \"{}\"", self.channel.to_string()), &[]).unwrap();
             loop {
-                let mut data_to_delete : Vec<i64> = Vec::new();
+                let mut data_to_delete : Vec<i32> = Vec::new();
                 let sql = &format!("SELECT id, {} FROM {}", self.column_name, self.table_name);
                 for next_row in &conn.query(sql, &[]).unwrap() {
                     let id: i32 = next_row.get(0);
                     let data: String = next_row.get(1);
                     let bin_data : &[u8] = data.as_bytes();
                     consumer.write(stream::SourceElement{id: Box::from("123"), data: Box::from(bin_data)});
-                    data_to_delete.push(id as i64);
+                    data_to_delete.push(id);
                 }
-                consumer.flush();
-                if !data_to_delete.is_empty() {
-                    conn.execute("DELETE FROM {} WHERE id IN ($1)", &[&data_to_delete]).unwrap();
-                }
-                data_to_delete.clear();
-                for i in 0..1000 {
+                self.flush_consumer(&mut data_to_delete, consumer, &conn);
+                let start_time = SystemTime::now();
+                loop {
+                    if start_time.elapsed().unwrap() >= NOTIFY_TIMEOUT {
+                        break;
+                    }
                     let a = it.next();
                     match a {
                         Ok(b) => {
                             if let Some(x) = b {
                                 let json_payload : Value = serde_json::from_str(&x.payload).unwrap();
-                                let string_payload: String = json_payload["payload"].to_string();
-                                let id : i64 = json_payload["id"].as_i64().unwrap();
+                                let string_payload: &str = json_payload["payload"].as_str().unwrap();
+                                println!("dino {} -> {}", json_payload, string_payload);
+                                let id : i32 = json_payload["id"].as_i64().unwrap() as i32;
                                 consumer.write(stream::SourceElement{id: Box::from("123"), data: Box::from(string_payload.as_bytes())});
                                 data_to_delete.push(id);
+                            } else {
+                                dbg!("No such value even if the result was ok");
+                                self.flush_consumer(&mut data_to_delete, consumer, &conn);
                             }
                         },
                         _ => {
@@ -86,10 +82,7 @@ pub mod pgsql {
                         }
                     }
                 }
-                if !data_to_delete.is_empty() {
-                    conn.execute("DELETE FROM {} WHERE id IN ($1)", &[&data_to_delete]).unwrap();
-                }
-                data_to_delete.clear();
+                self.flush_consumer(&mut data_to_delete, consumer, &conn);
             }
         }
     }
