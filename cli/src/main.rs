3a848bc8f64d08275b44f5235efda84a2eeff9f1
fcracker79 <fcracker79@gmail.com>
2019-08-01 19:36:58 +0200

    Added YAML configuration option

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -10,60 +10,86 @@ fn main() {
                           .version("0.0.1")
                           .author("Mirko Bonasorte <fcracker79@gmail.com>")
                           .about("Moves data from a PostgreSQL table to Kafka topic using LISTEN/NOTIFY mechanisms")
+                          .arg(Arg::with_name("yaml_file")
+                               .short("-y")
+                               .long("yaml-file")
+                               .value_name("YAML_FILE")
+                               .help(
+"YAML file with the following structure:
+    configurations:
+    - 
+      pgurl: a_postgresql_url
+      buffer_size: 12345
+      notify_timeout: 67890
+      kafka_brokers:
+          - kafka_broker1
+          - kafka_broker2
+    - ...
+")           
+                               .takes_value(true))
                           .arg(Arg::with_name("pg_url")
                                .short("-p")
                                .long("pgurl")
                                .value_name("PG_URL")
                                .help("PostGreSQL URL (default: postgres://postgres@localhost:5433)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("kafka_urls")
                                .short("-k")
                                .long("kafka-urls")
                                .value_name("PG_URL")
                                .help("Kafka URLs (default: localhost:9092)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("table_name")
                                .short("-t")
                                .long("table-name")
                                .value_name("TABLE_NAME")
                                .help("PostGreSQL Table name (default: events)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("column_name")
                                .short("-c")
                                .long("column-name")
                                .value_name("COLUMN_NAME")
                                .help("PostGreSQL Table column name (default: payload)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("channel")
                                .short("-z")
                                .long("channel-name")
                                .value_name("CHANNEL_NAME")
                                .help("PostGreSQL channel name (default: events.activity)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("topic")
                                .short("-w")
                                .long("topic-name")
                                .value_name("TOPIC_NAME")
                                .help("Kafka topic name (default: events)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("buffer_size")
                                .short("-b")
                                .long("buffer-size")
                                .value_name("BUFFER_SIZE")
                                .help("Kafka buffer size after which messages are written (default: 100)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("notify_timeout")
                                .short("-x")
                                .long("notify-timeout")
                                .value_name("NOTIFY_TIMEOUT")
                                .help("PostGreSQL Listen timeout (ms, default: 3000)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("notify_timeout_total")
                                .short("-X")
                                .long("notify-timeout-total")
                                .value_name("NOTIFY_TIMEOUT_TOTAL")
                                .help("Timeout after which rows are processed using a standard query (ms, default: 60000)")
-                               .takes_value(true))
+                               .takes_value(true)
+                               .conflicts_with("yaml_file"))
                           .arg(Arg::with_name("log4rs_file")
                                .short("-l")
                                .long("log4rs-configuration")
@@ -75,6 +101,9 @@ fn main() {
     if let Some(log4rs_file) = matches.value_of("log4rs_file") {
         println!("Configuring logger using {}", log4rs_file);
         log4rs::init_file(log4rs_file, Default::default()).unwrap();
+    }
+    if let Some(yaml_file) = matches.value_of("yaml_file") {
+        return push_the_elephant::Worker::multi_run(yaml_file).expect("Could not multi run!");
     }
     let mut builder = push_the_elephant::WorkerBuilder::default();
     if let Some(pgurl) = matches.value_of("pg_url") {
